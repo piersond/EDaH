787807cf9705717fb137bab1996a2d6a3cf05165
Derek Pierson <drk.pierson@gmail.com>
2023-07-28 09:48:15 -0600

    Added script to build HMGZD database

---
+++ Tools/build_HMGZD_database.r
@@ -0,0 +1,36 @@
+# Author: Derek Pierson
+# Date created: July 28, 2023
+
+# Script purpose is to bind together all HMGZD data found in the sub-directories
+# of the specified parent folder
+#------------------------------------------------------------------------------
+
+# USER INPUT
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+# Set parent directory
+parent_dir <- "C:/Users/DerekPierson/Box/ltspWorkspace/ltspData"
+
+# Set database output path
+db_output_path <- "C:/Users/DerekPierson/Box/ltspWorkspace/harmonizedDatabase"
+
+#-------------------------------------------------------------------------------
+# Then run script --------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+library(data.table)
+
+# Find all key file paths
+hmgzd_files <- list.files(path = parent_dir, 
+                               pattern = "HMGZD_data_output", 
+                               recursive = TRUE,
+                               full.names = TRUE)
+
+# Bind all HMGZD output data into one database
+HMGZD_db <- rbindlist(lapply(hmgzd_files, fread), fill=TRUE)
+
+# Export database as .csv and .rds
+write.csv(HMGZD_db, paste0(db_output_path, "/HMGZD_database_", Sys.Date(), ".csv"))
+saveRDS(HMGZD_db, paste0(db_output_path, "/HMGZD_database_", Sys.Date(), ".rds"))
